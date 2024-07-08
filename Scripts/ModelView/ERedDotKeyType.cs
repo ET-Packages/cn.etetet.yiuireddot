e777875ba778efe9117d092c2e3812d862719172
Lsy <379338943@qq.com>
2024-07-08 22:39:54 +0800

    新增测试数据

--- Scripts/ModelView/ERedDotKeyType.cs
+++ Scripts/ModelView/ERedDotKeyType.cs
@@ -11,5 +11,28 @@ namespace ET.Client
     {
         [LabelText("无")]
         public const int None = 0;
+
+        [LabelText("主")]
+        public const int Key1 = 1;
+
+        [LabelText("商店")]
+        public const int Key2 = 2;
+
+        [LabelText("钻石")]
+        public const int Key3 = 3;
+
+        [LabelText("金币")]
+        public const int Key4 = 4;
+
+        [LabelText("装备")]
+        public const int Key5 = 5;
+
+        [LabelText("强化")]
+        public const int Key6 = 6;
+
+        [LabelText("升级")]
+        public const int Key7 = 7;
+
+
     }
 }
